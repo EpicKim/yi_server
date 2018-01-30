58c0192d2a7161f0d642e6a96eb273c3580a4e5f
zourong <zourong@zourongdeMacBook-Pro.local>
2018-01-30 20:59:51 +0800

    商品增加颜色和尺寸属性(以后需要单独拎出来)

--- app/Admin/Controllers/ProductController.php
+++ app/Admin/Controllers/ProductController.php
@@ -25,9 +25,10 @@ class ProductController extends Controller
 
     public $states
         = [
-            'on'  => ['value' => 1, 'text' => '上架', 'color' => 'success'],
+            'on' => ['value' => 1, 'text' => '上架', 'color' => 'success'],
             'off' => ['value' => 0, 'text' => '下架', 'color' => 'danger'],
         ];
+
     /**
      * Index interface.
      *
@@ -88,30 +89,43 @@ class ProductController extends Controller
 
             $grid->id('ID')->sortable();
             $grid->name('商品名称')->sortable();
-            $grid->category_id('分类')->display(function($category_id){
-                $data=CategoryModel::find($category_id);
-                if($data){
+            $grid->category_id('分类')->display(function ($category_id) {
+                $data = CategoryModel::find($category_id);
+                if ($data) {
                     return $data->name;
                 }
                 return '';
             });
-            $grid->factory_id('厂家')->display(function($factory_id){
-                $data=FactoryModel::find($factory_id);
-                if($data){
+            $grid->factory_id('厂家')->display(function ($factory_id) {
+                $data = FactoryModel::find($factory_id);
+                if ($data) {
                     return $data->name;
                 }
                 return '';
             });
             $grid->price('现价')->sortable();
             $grid->original_price('原价')->sortable();
-            $grid->is_show('是否上架')->display(function($is_show){
-                if($is_show==1){
+            $grid->is_show('是否上架')->display(function ($is_show) {
+                if ($is_show == 1) {
                     return '上架';
                 }
                 return '下架';
             });
+
+            $grid->color('颜色')->display(function ($color) {
+                if (strlen($color) >= 8) {
+                    return mb_substr($color, 0, 8) . '...';
+                }
+                return $color;
+            });
+            $grid->size('尺寸')->display(function($size){
+               if(strlen($size)>=8){
+                   return mb_substr($size,0,8).'...';
+               }
+               return $size;
+            });
+
             $grid->created_at('创建时间')->sortable();
-            $grid->updated_at('修改时间')->sortable();
 
 
             $grid->disableExport();
@@ -129,13 +143,15 @@ class ProductController extends Controller
         return Admin::form(ProductModel::class, function (Form $form) {
 
             $form->display('id', 'ID');
-            $form->select('category_id','分类')->options('/admin/api/getcates');
-            $form->select('factory_id','厂家')->options('/admin/api/getfactories');
-            $form->text('name','商品名称');
-            $form->switch('is_show','是否上架')->states($this->states)->default(1);
-            $form->text('price','现价');
-            $form->text('original_price','原价');
-            $form->multipleImage('img','商品图片')->removable();
+            $form->select('category_id', '分类')->options('/admin/api/getcates');
+            $form->select('factory_id', '厂家')->options('/admin/api/getfactories');
+            $form->text('name', '商品名称');
+            $form->switch('is_show', '是否上架')->states($this->states)->default(1);
+            $form->text('price', '现价');
+            $form->text('original_price', '原价');
+            $form->text('size', '尺寸(以||分割)')->default('S||M||L||XL');
+            $form->text('color', '颜色(以||分割)');
+            $form->multipleImage('img', '商品图片')->removable();
 
             $form->display('created_at', '创建时间');
             $form->display('updated_at', '修改时间');
@@ -143,52 +159,65 @@ class ProductController extends Controller
     }
 
 
-    public function store(Request $request){
+    public function store(Request $request)
+    {
 
-        $category_id=$request->input('category_id');
-        $factory_id=$request->input('factory_id');
-        $name=$request->input('name');
-        $is_show=$request->input('is_show')=='on'?1:0;
-        $price=$request->input('price');
-        $original_price=$request->input('original_price');
-        $time=date('Y-m-d H:i:s');
-        $updated_at=$time;
-        $created_at=$time;
+        $category_id = $request->input('category_id');
+        $factory_id = $request->input('factory_id');
+        $name = $request->input('name');
+        $is_show = $request->input('is_show') == 'on' ? 1 : 0;
+        $price = $request->input('price');
+        $original_price = $request->input('original_price');
+        $time = date('Y-m-d H:i:s');
+        $updated_at = $time;
+        $created_at = $time;
 
-        $model=new ProductModel();
-        $model->category_id=$category_id;
-        $model->factory_id=$factory_id;
-        $model->name=$name;
-        $model->is_show=$is_show;
-        $model->price=$price;
-        $model->original_price=$original_price;
-        $model->updated_at=$updated_at;
-        $model->created_at=$created_at;
+        //尺寸和颜色
+        $size = trim($request->input('size', ''), '||');
+        $color = trim($request->input('color', ''), '||');
+        if ($size == '' || count(explode($size, '||')) == 0) {
+            $size = '';
+        }
+        if ($color == '' || count(explode($color, '||')) == 0) {
+            $color = '';
+        }
 
-        if($model->save()){
+        $model = new ProductModel();
+        $model->category_id = $category_id;
+        $model->factory_id = $factory_id;
+        $model->name = $name;
+        $model->is_show = $is_show;
+        $model->price = $price;
+        $model->original_price = $original_price;
+        $model->updated_at = $updated_at;
+        $model->created_at = $created_at;
+        $model->size = $size;
+        $model->color = $color;
+
+        if ($model->save()) {
             //保存成功,上传图片并写入product_img
-            $product_id=$model->id;
+            $product_id = $model->id;
 
-            $pic   = $request->file('img');
-            if($pic){
-                if(is_array($pic)){
-                    $sql_pre='insert into product_img(product_id,img,thumb_img,updated_at,created_at) values';
-                    $sql_val='';
-                    foreach($pic as $val){
-                        $data=$this->uploadImages($val);
-                        if($data){
-                            $sql_val.=",($product_id,'{$data[0]}','{$data[1]}','$time','$time')";
+            $pic = $request->file('img');
+            if ($pic) {
+                if (is_array($pic)) {
+                    $sql_pre = 'insert into product_img(product_id,img,thumb_img,updated_at,created_at) values';
+                    $sql_val = '';
+                    foreach ($pic as $val) {
+                        $data = $this->uploadImages($val);
+                        if ($data) {
+                            $sql_val .= ",($product_id,'{$data[0]}','{$data[1]}','$time','$time')";
                         }
                     }
-                    if($sql_val){
-                        $sql_val=substr($sql_val,1);
-                        DB::insert($sql_pre.$sql_val);
+                    if ($sql_val) {
+                        $sql_val = substr($sql_val, 1);
+                        DB::insert($sql_pre . $sql_val);
                     }
 
-                }else{
-                    $data=$this->uploadImages($pic);
-                    if(!$data){
-                        $sql="insert into product_img(product_id,img,thumb_img,updated_at,created_at) 
+                } else {
+                    $data = $this->uploadImages($pic);
+                    if (!$data) {
+                        $sql = "insert into product_img(product_id,img,thumb_img,updated_at,created_at) 
                           values($product_id,'{$data[0]}','{$data[1]}','$time','$time')";
                         DB::insert($sql);
                     }
@@ -208,26 +237,39 @@ class ProductController extends Controller
      * @param $id
      * @return string
      */
-    public function update(Request $request, $id){
-        $category_id=$request->input('category_id');
-        $factory_id=$request->input('factory_id');
-        $name=$request->input('name');
-        $is_show=$request->input('is_show')=='on'?1:0;
-        $price=$request->input('price');
-        $original_price=$request->input('original_price');
-        $time=date('Y-m-d H:i:s');
-        $updated_at=$time;
-        $created_at=$time;
+    public function update(Request $request, $id)
+    {
+        $category_id = $request->input('category_id');
+        $factory_id = $request->input('factory_id');
+        $name = $request->input('name');
+        $is_show = $request->input('is_show') == 'on' ? 1 : 0;
+        $price = $request->input('price');
+        $original_price = $request->input('original_price');
+        $time = date('Y-m-d H:i:s');
+        $updated_at = $time;
+        $created_at = $time;
 
-        $model=ProductModel::find($id);
-        $model->category_id=$category_id;
-        $model->factory_id=$factory_id;
-        $model->name=$name;
-        $model->is_show=$is_show;
-        $model->price=$price;
-        $model->original_price=$original_price;
-        $model->updated_at=$updated_at;
-        $model->created_at=$created_at;
+        //尺寸和颜色
+        $size = trim($request->input('size', ''), '||');
+        $color = trim($request->input('color', ''), '||');
+        if ($size == '' || count(explode($size, '||')) == 0) {
+            $size = '';
+        }
+        if ($color == '' || count(explode($color, '||')) == 0) {
+            $color = '';
+        }
+
+        $model = ProductModel::find($id);
+        $model->category_id = $category_id;
+        $model->factory_id = $factory_id;
+        $model->name = $name;
+        $model->is_show = $is_show;
+        $model->price = $price;
+        $model->original_price = $original_price;
+        $model->updated_at = $updated_at;
+        $model->created_at = $created_at;
+        $model->size = $size;
+        $model->color = $color;
 
         $model->save();
         admin_toastr(trans('admin.save_succeeded'));
@@ -236,22 +278,23 @@ class ProductController extends Controller
 
     }
 
-    public function destroy($id){
+    public function destroy($id)
+    {
 
         //删除product_img,product中数据并删除oss上对应的图片
-        $data=DB::select("select img,thumb_img from product_img where product_id=$id");
+        $data = DB::select("select img,thumb_img from product_img where product_id=$id");
         ProductModel::destroy($id);
-        if($data){
-            foreach($data as $val){
-                $img=$val->img;
-                $thumb_img=$val->thumb_img;
+        if ($data) {
+            foreach ($data as $val) {
+                $img = $val->img;
+                $thumb_img = $val->thumb_img;
                 Oss::deleteObject(config('alioss.product_dir') . CategoryModel::getFileName($img));
                 Oss::deleteObject(config('alioss.product_dir') . CategoryModel::getFileName($thumb_img));
             }
-            DB::delete("delete from product_img where product_id=?",[$id]);
+            DB::delete("delete from product_img where product_id=?", [$id]);
         }
         return response()->json([
-            'status'  => true,
+            'status' => true,
             'message' => trans('admin.delete_succeeded'),
         ]);
 
@@ -263,12 +306,13 @@ class ProductController extends Controller
      * @param $pic
      * @return array|bool
      */
-    protected function uploadImages($pic){
+    protected function uploadImages($pic)
+    {
         $ext = $pic->getClientOriginalExtension();
         if ($ext == 'jpg') {
             $ext = 'jpeg';
         }
-        $key  = time() . uniqid() . '.' . $ext;
+        $key = time() . uniqid() . '.' . $ext;
         $res1 = Oss::publicUpload(config('alioss.BucketName'), config('alioss.product_dir') . $key, $pic, [
             'ContentType' => 'application/' . $ext,
         ]);
@@ -283,9 +327,9 @@ class ProductController extends Controller
         unlink($tmp_pic);
 
         if ($res1 && $res2) {
-            $filepath   = config('alioss.prefix') . config('alioss.product_dir') . $key;
-            $thumb_filepath   = config('alioss.prefix') . config('alioss.product_dir') . $key2;
-            return [$filepath,$thumb_filepath];
+            $filepath = config('alioss.prefix') . config('alioss.product_dir') . $key;
+            $thumb_filepath = config('alioss.prefix') . config('alioss.product_dir') . $key2;
+            return [$filepath, $thumb_filepath];
         }
 
         return false;
